d8de4739c421ae956b863c9a831e8cb26b0e2389
codingbjs <codingbjs@gmail.com>
2023-09-04 11:43:06 +0900

    버블 정렬 구현

--- brute_force/bubble_sort.py
+++ brute_force/bubble_sort.py
@@ -1,2 +1,34 @@
-def sort(arr):
-    pass
+def bubble_sort(arr):
+    while True:
+        swap = False
+        i = 0
+        while i < len(arr) - 1:
+            print(arr)
+            if arr[i] > arr[i + 1]:
+                swap = True
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                # temp = arr[i + 1]
+                # arr[i + 1] = arr[i]
+                # arr[i] = temp
+            i += 1
+
+        if not swap:
+            return arr
+
+
+def liner_sort(arr):
+    i = 0
+    while i < len(arr) - 1:
+        j = i + 1
+        while j < len(arr):
+            if arr[i] > arr[j]:
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            j += 1
+        i += 1
+
+    return arr
+
+
+print(bubble_sort([5, 1, 4, 2, 8]))
---
+++ brute_force/test/TestSort.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestSort(unittest.TestCase):
+    def test_bibble_sort(self):
+        print()
+
+
+if __name__ == '__main__':
+    unittest.main()
